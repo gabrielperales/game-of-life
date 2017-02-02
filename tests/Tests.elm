adc73158b16a5462f78729fb371d3a34d17f8553
Gabriel Perales <gabrielperales@gmail.com>
2017-02-02 20:10:07 +0100

    added test to empty and isAlive funtions

--- tests/Tests.elm
+++ tests/Tests.elm
@@ -3,14 +3,39 @@ module Tests exposing (..)
 import Test exposing (..)
 import Expect
 import GameOfLife exposing (..)
+import Array
+import List
 
 
 all : Test
 all =
-    describe "Sample Test Suite"
-        [ describe "Unit test examples"
-            [ test "Addition" <|
+    describe "Game of life"
+        [ describe "empty"
+            [ test "should create cells X * Y cells" <|
                 \() ->
-                    Expect.equal (3 + 7) 10
+                    Expect.equal (empty ( 10, 10 ) |> .cells |> Array.length) 100
+            , test "all the cells should be false" <|
+                \() ->
+                    Expect.true "All cells are false" (empty ( 10, 10 ) |> .cells |> Array.toList |> List.all ((==) False))
+            , test "should store the board shape inside property shape" <|
+                \() ->
+                    Expect.equal (empty ( 10, 10 ) |> .shape) ( 10, 10 )
+            ]
+        , describe "isAlive"
+            [ test "Alive cell will die if has less than 2 neighbors by underpopulation" <|
+                \() ->
+                    Expect.false "Cell died" (isAlive True 1)
+            , test "Alive cell will survive if it has 2 heighbors" <|
+                \() ->
+                    Expect.true "Cell survive" (isAlive True 2)
+            , test "Alive cell will survive if it has 3 heighbors" <|
+                \() ->
+                    Expect.true "Cell survive" (isAlive True 3)
+            , test "Alive cell will die if it has more then 3 neighbors by overcrowding" <|
+                \() ->
+                    Expect.false "Cell died" (isAlive True 5)
+            , test "A died cell will born if it has 3 neighbors" <|
+                \() ->
+                    Expect.true "Cell born" (isAlive False 3)
             ]
         ]
